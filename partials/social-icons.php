5c3f5347ca7355152240a761b2dc00ec576031e1
Ash - SIlverless <ash@silverless.co.uk>
2020-02-18 12:27:01 +0000

    Add noreferrer to homepage

--- partials/social-icons.php
+++ partials/social-icons.php
@@ -3,7 +3,7 @@
   	<?php if ( get_field( 'social_icons_twitter', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_twitter', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_twitter', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-twitter">
 <span style="display:none">Twitter</span>
                 </i>
@@ -15,7 +15,7 @@
     <?php if ( get_field( 'social_icons_youtube', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_youtube', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_youtube', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-youtube"><span style="display:none">You Tube</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -25,7 +25,7 @@
     <?php if ( get_field( 'social_icons_pinterest', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_pinterest', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_pinterest', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-pinterest"><span style="display:none">Pinterest</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -35,7 +35,7 @@
     <?php if ( get_field( 'social_icons_facebook', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_facebook', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_facebook', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-facebook"><span style="display:none">Facebook</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -45,7 +45,7 @@
     <?php if ( get_field( 'social_icons_instagram', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_instagram', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_instagram', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-instagram"><span style="display:none">Instagram</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -55,7 +55,7 @@
     <?php if ( get_field( 'social_icons_google', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_google', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_google', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-google"><span style="display:none">Google+</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -65,7 +65,7 @@
     <?php if ( get_field( 'social_icons_linkedin', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_linkedin', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_linkedin', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-linkedin"><span style="display:none">LinkedIn</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
@@ -75,7 +75,7 @@
     <?php if ( get_field( 'social_icons_rss', 'options' ) ): ?>
 
 	  	<li class="social-icons__item">
-			<a href="<?php the_field( 'social_icons_rss', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="nofollow" aria-label="Social Link">
+			<a href="<?php the_field( 'social_icons_rss', 'options' ); ?>" class="social-icons__item__social-icon-link d-flex align-items-center justify-content-center" target="_blank" rel="noreferrer" aria-label="Social Link">
 				<i class="fa fa-rss"><span style="display:none">RSS</span></i>
 			</a>
 	    </li><!-- .social-icons__item -->
